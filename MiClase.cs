61ff92f839272a2fdd2d9ef039884ff834083ea1
Jonathan <jonatanmerche123@gmail.com>
2025-09-17 23:00:02 -0600

    Initial commit

---
+++ MiClase.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace examen
+{
+    public class MiClase
+    {
+        public double PromedioPositivos(double[] numeros)
+        {
+            double suma = 0;
+            int contador = 0;
+
+            for(int i = 0; i < numeros.Length; i++)
+            {
+                if (numeros[i] > 0)
+                {
+                    suma += numeros[i];
+                    contador ++;
+                }
+            }
+
+            if (contador == 0)
+            {
+                Console.WriteLine("No hay numeros positivos");
+                return 0;
+            }
+
+            double promedio = suma / contador;
+
+            return promedio;
+        }
+
+        public double PromedioNegativos(double[] numeros)
+        {
+            double suma_negativos = 0;
+            int contador_negativos = 0;
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                if (numeros[i] < 0)
+                {
+                    suma_negativos += numeros[i];
+                    contador_negativos++;
+                }
+            }
+
+            if (contador_negativos == 0)
+            {
+                Console.WriteLine("No hay numeros negativos");
+                return 0;
+            }
+
+            double promedio_negativos = suma_negativos / contador_negativos;
+            return promedio_negativos;
+        }
+
+        public double NumerosMayores(double[] numeros)
+        {
+            int contador_mayores = 0;
+
+            for (int i = 0;i < numeros.Length; i++)
+            {
+                if(numeros[i] > 100)
+                {
+                    contador_mayores++;
+                }
+            }
+
+            return contador_mayores;
+        }
+
+        public double NumerosMenores(double[] numeros)
+        {
+            int contador_menores = 0;
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                if (numeros[i] <= 0)
+                {
+                    contador_menores++;
+                }
+            }
+
+            return contador_menores;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿namespace examen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MiClase Operaciones = new MiClase();
+
+            double[] numeros = new double[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("\nIngrese un numero decimal: ");
+
+                try
+                {
+                    numeros[i] = double.Parse(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Ingrese solamente numeros.");
+                    i--;
+                }
+                
+
+            }
+
+            int opcion = 0;
+            double resultado = 0;
+            do
+            {
+                Console.WriteLine("\n---Menu---");
+                Console.WriteLine("1.Calcular el promedio de los numeros positivos");
+                Console.WriteLine("2.Calcular el promedio de los numeros negativos");
+                Console.WriteLine("3.Contar cuantos numeros son mayores a 100");
+                Console.WriteLine("4.Contar cuantos numeros son menores o iguales a 0");
+                Console.WriteLine("5.Salir");
+
+                Console.Write("\nIngrese una opcion: ");
+
+                try
+                {
+                    opcion = int.Parse(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Por favor ingresar una opcion valida del menu.");
+                    continue;
+                }
+
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("\n---Promedio de los numeros positivo---");
+                        resultado = Operaciones.PromedioPositivos(numeros);
+                        Console.WriteLine($"El promedio de los numeros positivos es: {resultado}");
+                        break;
+                    case 2:
+                        Console.WriteLine("\n---Promedio de los numeros negativos---");
+                        resultado = Operaciones.PromedioNegativos(numeros);
+                        Console.WriteLine($"El promedio de los numeros negativos es: {resultado}");
+                        break;
+                    case 3:
+                        Console.WriteLine("\n---Numeros mayores a 100---");
+                        resultado = Operaciones.NumerosMayores(numeros);
+                        Console.WriteLine($"La cantidad de numeros mayores a 100 que ingreso son: {resultado}");
+                        break;
+                    case 4:
+                        Console.WriteLine("\n---Numero menores o iguales a 0---");
+                        resultado = Operaciones.NumerosMenores(numeros);
+                        Console.WriteLine($"La cantidad de numeros menores o igual a cero que ingreso son: {resultado}");
+                        break;
+                    case 5:
+                        Console.WriteLine("\nNos vemos...");
+                        break;
+                    default:
+                        Console.WriteLine("Opcion no valida. Intente nuevamente. ");
+                        break;
+
+                }
+            
+
+            } while (opcion != 5);
+        }
+    }
+}
